a2759d47aa5aeb0f22dcabd0f5fc4501f90082a7
Angela Fan <angela.h.fan@gmail.com>
2019-02-26 11:59:17 -0800

    added introduction to lab2

---
+++ lab2/lab2.py
@@ -0,0 +1,24 @@
+# Lab 2
+# Chit-chat ChatBots
+
+# In the previous lab, we explored models that try to answer questions
+# by reasoning over free-text inputself.
+
+# In this lab, we will explore two types of models to create chatbots.
+
+# First, let's consider important qualities for a chatbot system
+# 1. Readability - whatever model we use, the chats it creates should be
+# easily understood by humans
+
+# 2. Consistency - when chatting with a chatbot, the bot should maintain
+# consistent information. Imagine a bot that says ``Hi I'm Jack'' and then
+# ``Hello, my name is Jane'' - quite confusing
+
+# 3. Engaging - To encourage users to talk to the bot, the bot should be
+# able to generate interesting, engaging responses. If the only response was
+# ``wow, that's cool,'' users are quite unlikely to want to talk very much
+# to the chat bot
+
+
+# DATA
+#
