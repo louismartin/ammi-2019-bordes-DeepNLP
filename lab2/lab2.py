6035ab46f8b102374c6fd004bd46003199b19c74
Angela Fan <angela.h.fan@gmail.com>
2019-02-26 12:13:35 -0800

    data and model intro section

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -21,4 +21,42 @@
 
 
 # DATA
-#
+# The dataset we will use for this lab is called ``PersonaChat'' - it was
+# created to directly address problem 2. Each person talking in the dataset
+# has a personality, which helps maintain consistency in the dialogue
+
+# Let's download the data and take a look at some examples
+python examples/display_data.py --task personachat --datatype train
+
+# Questions to ask yourself:
+# What do the personalities look like?
+# What are some drawbacks of these personalities for addressing the problem of consistency?
+
+# Let's understand how much data we have. Try to compute the following:
+
+# 1. How many turns of data do we have?
+# In dialogue datasets, "amount of data" is measured in dialogue turns.
+# Each time there is a single line of dialogue, that is called a "turn"
+
+# 2. On average, how many lines form a persona?
+
+
+# MODELS
+
+# As discussed in the lecture, there are two main kinds of dialogue models
+
+# Retrieval Models analyze the current dialogue context
+# and try to find appropriate responses in the dataset
+
+# Generative Models analyze the current dialogue context
+# and try to write an answer, word by word, from left to right.
+# This can be thought of as an application of sequence-to-sequence models,
+# where the "encoder side" is the dialogue history and the "decoder side"
+# is the dialogue response your chatbot should generate
+
+# Questions to ask yourself:
+# Review the pros/cons of retrieval compared to generative models
+# Are there settings when you might want to use one over the other?
+# Compare a chit-chat application to something like booking a movie ticket-
+# would you want to use generative, retrieval, or something else to accomplish
+# that task?
